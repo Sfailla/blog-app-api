481fc39b819ff3ea81b044218089f3b9cccdb4e4
Stevo99 <Sfailla1983@gmail.com>
2020-06-30 18:52:14 -0400

    added server file to project

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+const helmet = require('helmet');
+
+const index = 'index routes';
+const users = 'user routes';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`port listening on ${port}`));
