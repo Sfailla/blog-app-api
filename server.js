e42f500e1905e597d05c2210c14aa99cabeb02d1
Stevo99 <Sfailla1983@gmail.com>
2020-07-01 23:24:12 -0400

    changed path to indexRoute file

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // SECURE ENV VARIABLES WITH DOTENV
-const dotenv = require('dotenv');
-dotenv.config();
+require('./config/index');
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
@@ -9,30 +8,30 @@ const logger = require('morgan');
 const path = require('path');
 const helmet = require('helmet');
 
-const index = 'index routes';
+const indexRoute = require('./routes/index/indexRoute');
 const users = 'user routes';
 
 const app = express();
 
-app.use(logger('combined'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(logger('dev'));
 app.use(helmet());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRoute);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	let err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,10 +39,15 @@ app.use(function(err, req, res, next) {
 	// render the error page
 	res.status(err.status || 500);
 	res.json({
+		message: err.message,
 		error: err
 	});
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`port listening on ${port}`));
+app.listen(port, () =>
+	console.log(
+		`port listening on ${port} \nNODE_ENV=${process.env.NODE_ENV}`
+	)
+);
