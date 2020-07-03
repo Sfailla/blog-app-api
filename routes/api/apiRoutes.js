4fac4d8ca0de1827da1f7f20d6ee188dba815192
Stevo99 <Sfailla1983@gmail.com>
2020-07-03 08:12:15 -0400

    added post route that writes to data.json

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -8,12 +8,14 @@ const router = express.Router();
 const DATA_FILE = path.join(__dirname, '..', '..', 'data.json');
 
 router.get('/', (req, res) => {
+	res.setHeader('Cache-Control', 'no-cache');
 	res.status(200).json({
 		message: 'welcome to api routes 🚂'
 	});
 });
 
 router.get('/projects', (req, res) => {
+	res.setHeader('Cache-Control', 'no-cache');
 	fs.readFile(DATA_FILE, (err, data) => {
 		if (err) throw err;
 		res.status(200).json(JSON.parse(data));
@@ -33,6 +35,7 @@ router.post('/projects', (req, res) => {
 		};
 		projects.push(newProject);
 		fs.writeFile(DATA_FILE, JSON.stringify(projects, null, 4), () => {
+			res.setHeader('Cache-Control', 'no-cache');
 			res.status(200).json(projects);
 		});
 	});
