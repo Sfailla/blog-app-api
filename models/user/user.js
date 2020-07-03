2ca7f2c1435bf99b8a46727b0e00bd9391212e93
Stevo99 <Sfailla1983@gmail.com>
2020-07-02 20:34:07 -0400

    created user model

--- models/user/user.js
+++ models/user/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
 	username: {
 		type: String,
 		required: true,
@@ -19,6 +19,4 @@ const userSchema = new mongoose.Schema({
 	}
 });
 
-const User = mongoose.model('user', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
