fd87ee401c8557df3e20afc6a80732b655a7b316
Stevo99 <Sfailla1983@gmail.com>
2020-08-08 09:56:18 -0400

    removed boilerplate

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,44 +1,16 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { Router } = require('express');
 
-const router = express.Router();
-// im using this as a fake database. So I can read write and delete without making
-// a mongoose connection.  Using readFile() and writeFile() and a JSON file.
-const DATA_FILE = path.join(__dirname, '..', '..', 'data.json');
+const router = Router();
 
 router.get('/', (req, res) => {
 	res.setHeader('Cache-Control', 'no-cache');
 	res.status(200).json({
-		message: 'welcome to api routes 🚂'
+		message: 'welcome to api routes 🚀'
 	});
 });
 
-router.get('/projects', (req, res) => {
-	res.setHeader('Cache-Control', 'no-cache');
-	fs.readFile(DATA_FILE, (err, data) => {
-		if (err) throw err;
-		res.status(200).json(JSON.parse(data));
-	});
-});
-
-router.post('/projects', (req, res) => {
-	fs.readFile(DATA_FILE, (err, data) => {
-		if (err) throw err;
-		let projects = JSON.parse(data);
-		let newProject = {
-			id: req.body.id,
-			title: req.body.title,
-			description: req.body.description,
-			author: req.body.author,
-			createdAt: Date.now()
-		};
-		projects.push(newProject);
-		fs.writeFile(DATA_FILE, JSON.stringify(projects, null, 4), () => {
-			res.setHeader('Cache-Control', 'no-cache');
-			res.status(200).json(projects);
-		});
-	});
+router.get('/users', (req, res) => {
+	res.send('this is /api/users route 😎');
 });
 
 module.exports = router;
