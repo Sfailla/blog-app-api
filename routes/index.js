ffd866c66fbf17527b69421b13755986115f9697
Stevo99 <Sfailla1983@gmail.com>
2020-08-08 11:13:37 -0400

    reorganized file structure and removed unnecessary files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// Here is the index file for all custom routes.  require() all custom routes
-// and export them into server.js file!
-const indexRoutes = require('./index/indexRoutes');
-const apiRoutes = require('./api/apiRoutes');
-
-module.exports = {
-	indexRoutes,
-	apiRoutes
-};
