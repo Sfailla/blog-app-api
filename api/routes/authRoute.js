47f17ffc437d579e4bd16fcb2a669a14301de4ac
Stevo99 <Sfailla1983@gmail.com>
2020-08-09 22:17:07 -0400

    development

---
+++ api/routes/authRoute.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+const router = Router();
+
+/**
+ * =============================
+ * 	USER AUTH ROUTES
+ * =============================
+ */
+// get all users
+// router.get('/users', authController.getAllUsers);
+
+// get specific user
+// router.get('/users/:id');
+
+// create users
+// router.post('/users/register', authController.registerUser);
+
+module.exports = router;
