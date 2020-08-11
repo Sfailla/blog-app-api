ae7b717918e2f270d0a2d3453268a6875cf4fde6
Stevo99 <Sfailla1983@gmail.com>
2020-08-10 22:20:39 -0400

    cleaned up serverJS file

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 // bring in env variables and all configuration
-require('./config/index');
+require('./api/config/index');
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
-
+const apiRoutes = require('./api/routes/user-routes');
 const {
 	errorHandler,
 	notFoundHandler
 } = require('./api/middleware/error-handler');
 
-const { makeMongooseConnection } = require('./config/index');
-const apiRoutes = require('./api/routes/authRoute');
-
 const app = express();
-makeMongooseConnection();
 
 app.use(logger('dev'));
 app.use(helmet());
@@ -26,16 +22,19 @@ app.use(bodyParser.json());
 // uncomment this line whenever your ready for client code
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1', apiRoutes);
+
+app.get('/', (req, res) =>
+	res.status(200).json({ message: 'hello world 🌎🚀 🎃🎃🔐🗝' })
+);
+
 // Error and 404 handler middleware
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const port = process.env.PORT || 3001;
-
-app.listen(port, () =>
-	console.log(
-		`port listening on ${port} \nNODE_ENV=${process.env.NODE_ENV}`
-	)
-);
+app.listen(process.env.PORT || 3001, () => {
+	const PORT = process.env.PORT || 3001;
+	const NODE_ENV = process.env.NODE_ENV;
+	console.log(`port listening on ${PORT} \nNODE_ENV=${NODE_ENV}`);
+});
 
 module.exports = app;
