aa259725efecb5de8cacdd0cfefad3a6be38ec0e
Stevo99 <Sfailla1983@gmail.com>
2020-08-11 10:14:23 -0400

    refactored mongoose connection func

--- api/config/db/config.js
+++ api/config/db/config.js
@@ -1,25 +1,24 @@
 const mongoose = require('mongoose');
 
 function makeMongooseConnection() {
+	const mongooseOptions = {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		connectTimeoutMS: 1000,
+		promiseLibrary: global.Promise
+	};
+
+	const userMessage = 'connection established to MLAB database';
+	const errorMessage = err =>
+		`${err.name} \n there is an error with mongoose connect: ${err.code} ${err.errmsg}`;
+
 	if (process.env.MONGOOSE_URL) {
 		return mongoose
-			.connect(process.env.MONGOOSE_URL, {
-				useNewUrlParser: true,
-				useFindAndModify: false,
-				useCreateIndex: true,
-				useUnifiedTopology: true,
-				connectTimeoutMS: 1000,
-				promiseLibrary: global.Promise
-			})
-			.then(() =>
-				console.log('connection established to MLAB database')
-			)
-			.catch(
-				err =>
-					new Error(
-						`${err.name} \n there is an error with mongoose connect: ${err.code} ${err.errmsg}`
-					)
-			);
+			.connect(process.env.MONGOOSE_URL, mongooseOptions)
+			.then(() => console.log(userMessage))
+			.catch(err => new Error(errorMessage(err)));
 	}
 
 	return {
