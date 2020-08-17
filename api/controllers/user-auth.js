49b7f98e61251a2f417f48a34e72256c68f4a713
Stevo99 <Sfailla1983@gmail.com>
2020-08-17 13:37:03 -0400

    devloping user-auth controlle

--- api/controllers/user-auth.js
+++ api/controllers/user-auth.js
@@ -1,34 +1,44 @@
+const { handleError } = require('../middleware/utils/http-error');
+
 module.exports = class AuthController {
 	constructor(database) {
 		this.db = database;
 	}
 
 	registerUser = async (req, res) => {
-		const { username, email, password } = req.body;
-		const { user, token } = await this.db.createUser(
-			username,
-			email,
-			password
-		);
+		try {
+			const { username, email, password } = req.body;
+			const { user, token } = await this.db.createUser(
+				username,
+				email,
+				password
+			);
 
-		await res
-			.header('x-auth-token', token)
-			.status(200)
-			.json({ user });
+			await res
+				.header('x-auth-token', token)
+				.status(201)
+				.json({ user });
+		} catch (error) {
+			console.log('caught err');
+			handleError(res, error);
+		}
 	};
 
 	loginUser = async (req, res) => {
-		const { email, password } = req.body;
+		try {
+			const { email, password } = req.body;
+			const { user, token } = await this.db.getUserByEmailAndPassword(
+				email,
+				password
+			);
 
-		const { user, token } = await this.db.getUserByEmailAndPassword(
-			email,
-			password
-		);
-
-		await res
-			.header('x-auth-token', token)
-			.status(200)
-			.json({ user });
+			await res
+				.header('x-auth-token', token)
+				.status(200)
+				.json({ user });
+		} catch (error) {
+			handleError(res, error);
+		}
 	};
 
 	getCurrentUser = async (req, res) => {};
