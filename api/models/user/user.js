da6de5fbc3622ede18b61d94fd92923a2502774f
Stevo99 <Sfailla1983@gmail.com>
2020-08-19 15:33:38 -0400

    added more validation and formating to schema

--- api/models/user/user.js
+++ api/models/user/user.js
@@ -1,57 +1,63 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		unique: true,
-		required: [ true, 'username is required' ]
-	},
-	email: {
-		type: String,
-		required: [ true, 'email is required' ],
-		validate: {
-			validator: value => {
-				return validator.isEmail(value);
-			},
-			message: '{VALUE} is not a valid email address'
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: [ true, 'username is required parameter' ]
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: [ true, 'email is required parameter' ],
+			set: value => value.toLowerCase(),
+			validate: {
+				validator: value => {
+					return validator.isEmail(value);
+				},
+				message: '{VALUE} is not a valid email address'
+			}
+		},
+		password: {
+			type: String,
+			required: true,
+			validate: {
+				validator: value => {
+					return !validator.isEmpty(value);
+				},
+				message: '{VALUE} is not a valid password'
+			}
+		},
+		role: {
+			type: String,
+			enum: [ 'user', 'admin' ],
+			default: 'user'
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			validate: {
+				validator: value => {
+					return validator.isDate(value);
+				},
+				message: '{VALUE} must be type date'
+			}
 		}
 	},
-	password: {
-		type: String,
-		required: true,
-		validate: {
-			validator: value => {
-				return !validator.isEmpty(value);
-			},
-			message: '{VALUE} cannot be empty'
-		}
-	},
-	role: {
-		type: String,
-		enum: [ 'user', 'admin' ],
-		default: 'user'
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-		validate: {
-			validator: value => {
-				return validator.isDate(value);
-			},
-			message: '{VALUE} must be type date'
+	{
+		toJSON: {
+			virtuals: true,
+			versionKey: false,
+			transform: (_, ret) => {
+				console.log(_);
+				delete ret._id;
+				delete ret.password;
+			}
 		}
 	}
-});
-
-UserSchema.set('toJSON', {
-	virtuals: true,
-	versionKey: false,
-	transform: (doc, ret) => {
-		delete ret._id;
-		delete ret.password;
-	}
-});
+);
 
 UserSchema.methods.isValidObjectId = function(userId) {
 	return (
