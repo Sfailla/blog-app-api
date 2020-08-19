d675bc7139589db0f986649103e5dd71c471d467
Stevo99 <Sfailla1983@gmail.com>
2020-08-19 19:48:43 -0400

    miner changes

--- api/middleware/route/requireAdmin.js
+++ api/middleware/route/requireAdmin.js
@@ -5,7 +5,7 @@ const requireAdmin = requiredRole => async (req, res, next) => {
 	try {
 		if (!requiredRole) {
 			const errMsg = 'must provide authorization role';
-			throw new UserServiceError(422, errMsg);
+			new UserServiceError(422, errMsg);
 		}
 
 		const { user_id, user_role } = req.user;
