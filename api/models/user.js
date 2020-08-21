706732a4e2a82990ca4913763fe7741aa891b86e
Stevo99 <Sfailla1983@gmail.com>
2020-08-21 09:35:50 -0400

    refactored user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,58 +1,68 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		unique: true,
-		required: [ true, 'username is required parameter' ]
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: [ true, 'email is required parameter' ],
-		set: value => value.toLowerCase(),
-		validate: {
-			validator: value => {
-				return validator.isEmail(value);
-			},
-			message: '{VALUE} is not a valid email address'
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		validate: {
-			validator: value => {
-				return !validator.isEmpty(value);
-			},
-			message: '{VALUE} is not a valid password'
-		}
-	},
-	role: {
-		type: String,
-		enum: [ 'user', 'admin' ],
-		default: 'user'
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-		validate: {
-			validator: value => {
-				return validator.isDate(value);
-			},
-			message: '{VALUE} must be type date'
-		}
-	}
-});
+const { Schema, model } = mongoose;
 
-UserSchema.set('toJSON', {
+const requiredString = {
+	type: String,
+	required: true
+};
+
+const options = {
 	virtuals: true,
 	versionKey: false,
 	transform: (_, ret) => {
 		delete ret._id;
 		delete ret.password;
 	}
-});
+};
 
-module.exports = mongoose.model('User', UserSchema);
+const UserSchema = new Schema(
+	{
+		username: {
+			...requiredString,
+			trim: true,
+			unique: true
+		},
+		email: {
+			...requiredString,
+			trim: true,
+			unique: true,
+			set: value => value.toLowerCase(),
+			validate: {
+				validator: value => {
+					return validator.isEmail(value);
+				},
+				message: '{VALUE} is not a valid email address'
+			}
+		},
+		password: {
+			...requiredString,
+			trim: true,
+			validate: {
+				validator: value => {
+					return !validator.isEmpty(value);
+				},
+				message: '{VALUE} is not a valid password'
+			}
+		},
+		role: {
+			type: String,
+			enum: [ 'user', 'admin' ],
+			default: 'user'
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			validate: {
+				validator: value => {
+					return validator.isDate(value);
+				},
+				message: '{VALUE} must be type date'
+			}
+		}
+	},
+	{ toJSON: options }
+);
+
+module.exports = model('User', UserSchema);
