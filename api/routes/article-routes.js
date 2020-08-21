815f20babeb1ca3a3d1edfaae85bc8215cad3391
Stevo99 <Sfailla1983@gmail.com>
2020-08-20 23:39:02 -0400

    added article routes

---
+++ api/routes/article-routes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+
+const ArticleController = require('../controllers/article');
+const ArticleDbService = require('../services/article-service');
+const ArticleModel = require('../models/article');
+const { authenticateJWT } = require('../middleware/index');
+
+const articleService = new ArticleDbService(ArticleModel);
+const articleController = new ArticleController(articleService);
+
+const router = Router();
+
+router.get('/', articleController.getArticles);
+
+router.post('/', authenticateJWT, articleController.createArticle);
+
+module.exports = router;
