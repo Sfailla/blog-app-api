8a275cc200a13b83764e04e25e601c645373025e
Stevo99 <Sfailla1983@gmail.com>
2020-08-23 09:26:50 -0400

    added tag query functionality to getArticles method

--- api/controllers/article.js
+++ api/controllers/article.js
@@ -1,20 +1,18 @@
 module.exports = class ArticleController {
-	constructor(databaseService, validationError) {
+	constructor(databaseService) {
 		this.service = databaseService;
-		this.articleError = (statusCode, message) => {
-			return new validationError(statusCode, message);
-		};
 	}
 
 	createArticle = async (req, res, next) => {
-		const { title, description, body } = req.body;
+		const { title, description, body, tagList } = req.body;
 		const { id } = req.user;
 		try {
 			const { article, err } = await this.service.createArticle(
 				id,
 				title,
 				description,
-				body
+				body,
+				tagList
 			);
 			if (err) throw err;
 			await res.status(200).json({ article });
@@ -23,18 +21,42 @@ module.exports = class ArticleController {
 		}
 	};
 
-	getAllArticles = async (req, res, next) => {
-		const { limit, offset } = req.query;
-		const { articles } = await this.service.getAllArticles(
-			limit,
-			offset
-		);
-		res.status(200).json({ articles });
+	getArticles = async (req, res, next) => {
+		const { limit, offset, tags, author, favorited } = req.query;
+		try {
+			const { articles, err } = await this.service.getAllArticles(
+				limit,
+				offset,
+				tags,
+				author,
+				favorited
+			);
+			if (err) throw err;
+			res.status(200).json({ articles });
+		} catch (error) {
+			next(error);
+		}
 	};
 
 	getArticlesByUser = async (req, res, next) => {
+		const { limit, offset } = req.query;
 		const { id } = req.user;
-		const { articles } = await this.service.getArticlesByUser(id);
-		res.status(200).json({ articles });
+		try {
+			const { articles, err } = await this.service.getArticlesByUser(
+				id,
+				limit,
+				offset
+			);
+			if (err) throw err;
+			res.status(200).json({ articles });
+		} catch (error) {
+			next(err);
+		}
+	};
+
+	getArticleBySlug = async (req, res, next) => {
+		const { slug } = req.body;
+		const { article } = await this.service.getArticleBySlug(slug);
+		res.status(200).json({ article });
 	};
 };
--- api/services/article-service.js
+++ api/services/article-service.js
@@ -1,18 +1,20 @@
-const { copyArticleObj } = require('../helpers/article');
+const { copyArticleObj, formatTags } = require('../helpers/article');
 const { isValidObjId } = require('../config/index');
 const { ValidationError } = require('../middleware/utils/errors');
 
 module.exports = class ArticleDatabaseService {
-	constructor(articleModel) {
+	constructor(articleModel, userModel) {
 		this.db = articleModel;
+		this.user = userModel;
 	}
 
-	createArticle = async (id, title, description, body) => {
+	createArticle = async (id, title, description, body, tagList) => {
 		let article = await this.db.create({
 			author: id,
 			title,
 			description,
-			body
+			body,
+			tagList
 		});
 		if (!article) {
 			const errMsg = 'error creating article';
@@ -23,14 +25,35 @@ module.exports = class ArticleDatabaseService {
 		return { article };
 	};
 
-	getAllArticles = async (limit = 10, offset = 0) => {
+	getAllArticles = async (
+		limit = 10,
+		offset = 0,
+		tags,
+		author,
+		favorited
+	) => {
 		const query = {};
 		const options = {
 			sort: { updatedAt: 'desc' },
 			limit: Number(limit),
 			skip: offset
 		};
+
+		if (tags) {
+			query['tagList'] = { $in: formatTags(tags) };
+		}
+
+		if (author) {
+			query['username'] = author;
+			const user = await this.user.findOne(query);
+			query['author'] = user._id;
+		}
+
+		console.log(query);
+
 		const articles = await this.db.find(query, null, options);
+		console.log(articles);
+
 		if (!articles) {
 			const errMsg = 'error fetching all articles';
 			const err = new ValidationError(400, errMsg);
@@ -42,15 +65,34 @@ module.exports = class ArticleDatabaseService {
 		return { articles: copyArticles };
 	};
 
-	getArticlesByUser = async userId => {
+	getArticlesByUser = async (userId, limit = 5, offset = 0) => {
+		const query = { author: userId };
+		const options = {
+			sort: { updatedAt: 'desc' },
+			limit: Number(limit),
+			skip: offset
+		};
 		if (isValidObjId(userId)) {
-			let articles = await this.db.find({ author: userId });
+			let articles = await this.db.find(query, null, options);
+			if (!articles) {
+				const errMsg = 'error fetching all user articles';
+				const err = new ValidationError(400, errMsg);
+				return { err };
+			}
 			const copyArticles = articles.map(article =>
 				copyArticleObj(article)
 			);
 			return { articles: copyArticles };
+		} else {
+			const errMsg = `invalid object id => ${userId}`;
+			const err = new ValidationError(400, errMsg);
+			return { err };
 		}
 	};
 
-	getArticleBySlug = () => {};
+	getArticleBySlug = async slug => {
+		const query = { slug };
+		const article = await this.db.findOne(query);
+		return { article };
+	};
 };
