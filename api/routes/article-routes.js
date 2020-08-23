0cf8b656cb99a0b32d77624f4b8667aa9a817bbe
Stevo99 <Sfailla1983@gmail.com>
2020-08-23 09:28:12 -0400

    changed name of getAllArticles to getArticles

--- api/routes/article-routes.js
+++ api/routes/article-routes.js
@@ -2,20 +2,18 @@ const { Router } = require('express');
 
 const ArticleController = require('../controllers/article');
 const ArticleDbService = require('../services/article-service');
-const ArticleModel = require('../models/article');
 
-const { ValidationError } = require('../middleware/utils/errors');
+const ArticleModel = require('../models/article');
+const UserModel = require('../models/user');
+
 const { authenticateJWT } = require('../middleware/index');
 
-const articleService = new ArticleDbService(ArticleModel);
-const articleController = new ArticleController(
-	articleService,
-	ValidationError
-);
+const articleService = new ArticleDbService(ArticleModel, UserModel);
+const articleController = new ArticleController(articleService);
 
 const router = Router();
 
-router.get('/', articleController.getAllArticles);
+router.get('/', articleController.getArticles);
 
 router.post('/', authenticateJWT, articleController.createArticle);
 
