fb25fd07dfcc16e30a8e0be5f63f6b2f135d356c
Stevo99 <Sfailla1983@gmail.com>
2020-08-23 22:12:56 -0400

    added randomBytes random Id to slugify functions

--- api/helpers/article.js
+++ api/helpers/article.js
@@ -1,16 +1,20 @@
+const { randomBytes } = require('crypto');
+
 const copyArticleObj = article => {
 	const {	id, author, slug,	title, description,	body,	comments,	tagList,	isFavorite,	favoriteCount,	updatedAt, createdAt } = article;
   return { id,	author,	slug,	title,	description,	body,	comments,	tagList,	isFavorite,	favoriteCount,	updatedAt,	createdAt
   }
 };
 
-const formatTags = (tags) => {
+const formatTags = tags => {
   return tags.split(',').map(tag => tag.toLowerCase());
 }
 
-const slugify = (slug) => {
-  return slug.toLowerCase().split(' ').join('-');
-}
+const slugify = slug => {
+	return `${slug.toLowerCase().split(' ').join('-')}-${randomBytes(
+		4
+	).toString('hex')}`;
+};
 
 module.exports = {
   copyArticleObj,
