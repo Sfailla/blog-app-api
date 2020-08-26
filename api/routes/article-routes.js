d68ca00007a32d03eb10c0404401f05a42ce4943
Stevo99 <Sfailla1983@gmail.com>
2020-08-25 22:51:34 -0400

    changed naming convention of some routes, added favoriteArticle route, organized require statements and variables

--- api/routes/article-routes.js
+++ api/routes/article-routes.js
@@ -1,11 +1,8 @@
 const { Router } = require('express');
-
 const ArticleController = require('../controllers/article');
 const ArticleDbService = require('../services/article-service');
-
 const ArticleModel = require('../models/article');
 const UserModel = require('../models/user');
-
 const { authenticateJWT } = require('../middleware/index');
 
 const articleService = new ArticleDbService(ArticleModel, UserModel);
@@ -16,19 +13,19 @@ const router = Router();
 router.post('/', authenticateJWT, articleController.createArticle);
 
 router.post(
-	'/:slug/favorite',
+	'/:article/favorite',
 	authenticateJWT,
-	articleController.setFavoriteArticle
+	articleController.favoriteArticle
 );
 
 router.get('/', articleController.getArticles);
 
+router.get('/:article', articleController.getArticle);
+
 router.get(
 	'/my-articles',
 	authenticateJWT,
-	articleController.getArticlesByUser
+	articleController.getUserArticle
 );
-
-router.get('/:slug', articleController.getArticleBySlug);
 
 module.exports = router;
