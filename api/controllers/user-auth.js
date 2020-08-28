89aff584eee55b68fdae26cc4473a73caa9428f2
Stevo99 <Sfailla1983@gmail.com>
2020-08-28 17:36:26 -0400

    added return statements to all await res and next() functions

--- api/controllers/user-auth.js
+++ api/controllers/user-auth.js
@@ -11,12 +11,12 @@ module.exports = class AuthController {
 				password: req.body.password
 			});
 			if (err) throw err;
-			await res
+			return await res
 				.header('x-auth-token', token)
 				.status(201)
 				.json({ user });
 		} catch (error) {
-			next(error);
+			return next(error);
 		}
 	};
 
@@ -28,29 +28,29 @@ module.exports = class AuthController {
 				req.body.password
 			);
 			if (err) throw err;
-			await res
+			return await res
 				.header('x-auth-token', token)
 				.status(200)
 				.json({ user });
 		} catch (error) {
-			next(error);
+			return next(error);
 		}
 	};
 
 	logoutUser = async (req, res, next) => {
-		await res.send('this is the logout route!');
+		return await res.send('this is the logout route!');
 	};
 
 	getCurrentUser = async (req, res, next) => {
 		try {
 			const { user, err } = await this.db.getUserById(req.params.id);
 			if (err) throw err;
-			await res
+			return await res
 				.header('x-auth-token', req.token)
 				.status(200)
 				.json({ user });
 		} catch (error) {
-			next(error);
+			return next(error);
 		}
 	};
 
@@ -58,12 +58,12 @@ module.exports = class AuthController {
 		try {
 			const { users, err } = await this.db.getAllUsers();
 			if (err) throw err;
-			await res
+			return await res
 				.header('x-auth-token', req.token)
 				.status(200)
 				.json({ users });
 		} catch (error) {
-			next(error);
+			return next(error);
 		}
 	};
 };
