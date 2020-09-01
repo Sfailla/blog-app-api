aafd5f244387da69aa49692660a1541dc36d2a3a
Stevo99 <Sfailla1983@gmail.com>
2020-08-31 23:14:59 -0400

    added fetchUserProfile and fetchFollwService to profile service.

--- api/services/profile-service.js
+++ api/services/profile-service.js
@@ -1,5 +1,27 @@
+const { makeUserProfile } = require('../helpers/user-auth');
+const { ValidationError } = require('../middleware/utils/errors');
+
 module.exports = class ProfileDatabaseService {
 	constructor(userModel) {
 		this.profile = userModel;
 	}
+
+	fetchUserProfile = async username => {
+		const query = { username };
+		const profile = await this.profile.findOne(query);
+		if (!profile) {
+			return {
+				err: new ValidationError(400, 'error fetching profile')
+			};
+		}
+		console.log('profile %s', profile);
+		return { profile: await makeUserProfile(profile) };
+	};
+
+	fetchFollowService = async authUser => {
+		const user = await this.profile.findOne({ _id: authUser.id });
+		const updatedUser = await user.follow(user._id);
+		console.log(updatedUser);
+		return { profile: await makeUserProfile(updatedUser) };
+	};
 };
