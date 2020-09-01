6fa743ab20dbd4b086ababf8f8c2dd86179ef31d
Stevo99 <Sfailla1983@gmail.com>
2020-08-31 23:11:31 -0400

    added getProfile and followUser methods

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -4,6 +4,23 @@ module.exports = class ProfileController {
 	}
 
 	getProfile = async (req, res, next) => {
-		// console.log(this.service);
+		try {
+			const { profile, err } = await this.service.fetchUserProfile(
+				req.params.username
+			);
+			console.log(profile);
+			if (err) throw err;
+			return await res.status(200).json({ profile });
+		} catch (error) {
+			return next(error);
+		}
+	};
+
+	followUser = async (req, res, next) => {
+		const { profile } = await this.service.fetchFollowService(
+			req.user
+		);
+		console.log(profile);
+		return await res.status(200).json({ profile });
 	};
 };
