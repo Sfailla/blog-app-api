1ad6f7690ddc38ae5fe768c02efef280b360d443
Stevo99 <Sfailla1983@gmail.com>
2020-09-07 20:08:42 -0400

    added toJson helper for comment model

--- api/models/comment.js
+++ api/models/comment.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose');
 const { Schema, Types, model } = mongoose;
 const { ObjectId } = Types;
 
-const CommentSchema = new Schema({
-	author: { type: ObjectId, ref: 'User' },
-	article: { type: ObjectId, ref: 'Article' },
-	comment: { type: String },
-	updatedAt: { type: Date, default: Date.now },
-	createdAt: { type: Date, default: Date.now }
-});
+const options = {
+	virtuals: true,
+	versionKey: false,
+	transform: (_, ret) => {
+		delete ret._id;
+	}
+};
+
+const CommentSchema = new Schema(
+	{
+		author: { type: ObjectId, ref: 'User' },
+		article: { type: ObjectId, ref: 'Article' },
+		comment: { type: String, required: true, trim: true },
+		updatedAt: { type: Date, default: Date.now },
+		createdAt: { type: Date, default: Date.now }
+	},
+	{ toJSON: options }
+);
 
 module.exports = model('Comment', CommentSchema);
