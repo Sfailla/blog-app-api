f8c746fa480faf3206fdf10aedda5e5bed287202
Stevo99 <Sfailla1983@gmail.com>
2020-09-09 09:26:43 -0400

    implemented deleteComment comment scemaa method

--- api/models/comment.js
+++ api/models/comment.js
@@ -21,4 +21,13 @@ const CommentSchema = new Schema(
 	{ toJSON: options }
 );
 
+CommentSchema.methods.deleteComment = async function(userId, commentId) {
+	if (this.author.toString() === userId.toString()) {
+		if (this._id.toString() === commentId.toString()) {
+			await this.remove(commentId);
+		}
+	}
+	return;
+};
+
 module.exports = model('Comment', CommentSchema);
