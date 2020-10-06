f780b2b3b5704b6897aa28545ed0b9719c11465a
Stevo99 <Sfailla1983@gmail.com>
2020-10-06 11:40:30 -0400

    changed verifyAuthToken to new verifyToken method

--- api/middleware/route/authenticate.js
+++ api/middleware/route/authenticate.js
@@ -1,11 +1,11 @@
-const { verifyAuthToken, makeUserObj } = require('../../helpers/user-auth');
+const { verifyToken, makeUserObj } = require('../../helpers/user-auth');
 const { ValidationError } = require('../utils/errors');
 const UserModel = require('../../models/user');
 
 const authenticateJWT = async (req, res, next) => {
 	try {
 		const token = req.header('x-auth-token');
-		const verifiedUser = await verifyAuthToken(token);
+		const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
 		const user = await UserModel.findById(verifiedUser.userId);
 
 		if (!verifiedUser || !user) {
