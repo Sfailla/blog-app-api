9946a4298f0e56699c8e272ab4630c380faae84b
Stevo99 <Sfailla1983@gmail.com>
2020-10-07 16:24:17 -0400

    updated catch block to catch JWT expired error and add status

--- api/middleware/route/authenticate.js
+++ api/middleware/route/authenticate.js
@@ -1,4 +1,4 @@
-const { verifyToken, makeUserObj } = require('../../helpers/user-auth');
+const { verifyToken, makeAuthUser } = require('../../helpers/user-auth');
 const { ValidationError } = require('../utils/errors');
 const UserModel = require('../../models/user');
 
@@ -7,16 +7,19 @@ const authenticateJWT = async (req, res, next) => {
 		const token = req.header('x-auth-token');
 		const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
 		const user = await UserModel.findById(verifiedUser.userId);
-
 		if (!verifiedUser || !user) {
 			const errMsg = 'error authenticating user';
 			throw new ValidationError(400, errMsg);
 		}
 
-		req.user = makeUserObj(user);
+		req.user = makeAuthUser(user);
 
 		return next();
 	} catch (error) {
+		if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
+			error.status = 403;
+			next(error);
+		}
 		next(error);
 	}
 };
