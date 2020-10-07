2653bd16e430e098f73346cdf9d402b883e1e505
Stevo99 <Sfailla1983@gmail.com>
2020-10-07 16:22:15 -0400

    changed requireAdmin to requiredRole in route middleware

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,8 +1,8 @@
 const authenticateJWT = require('./route/authenticate');
-const requireAdmin = require('./route/requireAdmin');
+const requiredRole = require('./route/requiredRole');
 
 // export our middlewares to prevent multiple imports
 module.exports = {
 	authenticateJWT,
-	requireAdmin
+	requiredRole
 };
