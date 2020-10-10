fc2972e001227e1a2e60fda0845a5e5dd18e504f
Stevo99 <Sfailla1983@gmail.com>
2020-10-10 11:21:21 -0400

    changed verifyToken id lookup from userId to id

--- api/middleware/route/authenticate.js
+++ api/middleware/route/authenticate.js
@@ -6,12 +6,11 @@ const authenticateJWT = async (req, res, next) => {
 	try {
 		const token = req.header('x-auth-token');
 		const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
-		const user = await UserModel.findById(verifiedUser.userId);
+		const user = await UserModel.findById(verifiedUser.id);
 		if (!verifiedUser || !user) {
 			const errMsg = 'error authenticating user';
 			throw new ValidationError(400, errMsg);
 		}
-
 		req.user = makeAuthUser(user);
 
 		return next();
