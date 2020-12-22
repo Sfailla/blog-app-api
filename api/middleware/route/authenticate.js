badb0049c8b84747b25b1770cd3de0e33d9fd368
Sfailla <Sfailla1983@gmail.com>
2020-12-22 17:07:50 -0500

    added prettier setting to workspace

--- api/middleware/route/authenticate.js
+++ api/middleware/route/authenticate.js
@@ -3,25 +3,25 @@ const { ValidationError } = require('../utils/errors');
 const UserModel = require('../../models/user');
 
 const authenticateJWT = async (req, res, next) => {
-	try {
-		const token = req.header('x-auth-token');
-		const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
-		const user = await UserModel.findById(verifiedUser.userId);
+  try {
+    const token = req.header('x-auth-token');
+    const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await UserModel.findById(verifiedUser.userId);
 
-		if (!verifiedUser || !user) {
-			const errMsg = 'error authenticating user';
-			throw new ValidationError(400, errMsg);
-		}
-		req.user = makeAuthUser(user);
+    if (!verifiedUser || !user) {
+      const errMsg = 'error authenticating user';
+      throw new ValidationError(400, errMsg);
+    }
+    req.user = makeAuthUser(user);
 
-		return next();
-	} catch (error) {
-		if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
-			error.status = 403;
-			next(error);
-		}
-		next(error);
-	}
+    return next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
+      error.status = 403;
+      next(error);
+    }
+    next(error);
+  }
 };
 
 module.exports = authenticateJWT;
