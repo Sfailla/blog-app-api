7bc89eb2610f21e3fcde8ce393040ae9e6165f66
Sfailla <Sfailla1983@gmail.com>
2021-08-08 11:41:02 -0400

    improvements to terminalColor code

--- config/terminalColors.js
+++ config/terminalColors.js
@@ -1,47 +1,44 @@
 // cool function I made for development to change terminal colors ✨🌈✨
 const colorTerminal = terminalColor => {
-	terminalColor = terminalColor.toLowerCase();
-	const acceptedColors = [ 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan' ];
+  terminalColor = terminalColor.toLowerCase()
+  const acceptedColors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
 
-	const RED = 31;
-	const GREEN = 32;
-	const YELLOW = 33;
-	const BLUE = 34;
-	const MAGENTA = 35;
-	const CYAN = 36;
+  const checkColorReturnCode = color => {
+    const colorMap = {
+      red: 31,
+      green: 32,
+      yellow: 33,
+      blue: 34,
+      magenta: 35,
+      cyan: 36
+    }
 
-	const checkColorReturnCode = color => {
-		if (color === 'red') return RED;
-		if (color === 'green') return GREEN;
-		if (color === 'yellow') return YELLOW;
-		if (color === 'blue') return BLUE;
-		if (color === 'magenta') return MAGENTA;
-		if (color === 'cyan') return CYAN;
-	};
+    return colorMap[color]
+  }
 
-	const verifiedColor = acceptedColors.find(color => color === terminalColor);
+  const verifiedColor = acceptedColors.find(color => color === terminalColor)
 
-	const colorModel = acceptedColors.reduce((obj, items) => {
-		if (verifiedColor) {
-			obj[items] = {
-				isColor: items === verifiedColor,
-				color: items,
-				colorCode: checkColorReturnCode(items)
-			};
-		} else {
-			obj['error'] = {
-				isColor: false,
-				message: `sorry ${terminalColor} is not available 😕\n`
-			};
-		}
-		return obj;
-	}, {});
+  const colorModel = acceptedColors.reduce((obj, items) => {
+    if (verifiedColor) {
+      obj[items] = {
+        isColor: items === verifiedColor,
+        color: items,
+        colorCode: checkColorReturnCode(items)
+      }
+    } else {
+      obj['error'] = {
+        isColor: false,
+        message: `sorry ${terminalColor} is not available 😕\n`
+      }
+    }
+    return obj
+  }, {})
 
-	const model = verifiedColor ? colorModel[verifiedColor] : colorModel['error'];
+  const model = verifiedColor ? colorModel[verifiedColor] : colorModel['error']
 
-	const { colorCode, message } = model;
+  const { colorCode, message } = model
 
-	return colorCode ? `\x1b[${colorCode}m%s\x1b[0m` : message;
-};
+  return colorCode ? `\x1b[${colorCode}m%s\x1b[0m` : message
+}
 
-module.exports = colorTerminal;
+module.exports = colorTerminal
